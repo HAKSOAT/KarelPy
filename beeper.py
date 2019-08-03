71a725a223b731eff604a48a3fcf84e9f56cd960
haksoat <shopejuh@gmail.com>
2019-08-03 13:21:33 +0100

    Renamed beeper file

--- beeper.py
+++ beeper.py
@@ -5,7 +5,7 @@ import config
 class Beeper(pygame.sprite.Sprite):
     def __init__(self, center):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("world/beezer.png").convert_alpha()
+        self.image = pygame.image.load("world/beeper.png").convert_alpha()
         self.image = pygame.transform.scale(self.image, (config.DIMENSION_UNIT//2, config.DIMENSION_UNIT//2))
         self.image = pygame.transform.rotate(self.image, 45)
         self.rect = self.image.get_rect()
