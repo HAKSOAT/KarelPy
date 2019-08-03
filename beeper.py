c48f2dc5c1c19aa1fa6ce8695d036cf3d6366e4f
haksoat <shopejuh@gmail.com>
2019-08-03 13:04:02 +0100

    Created beeper object

---
+++ beeper.py
@@ -0,0 +1,12 @@
+import pygame
+import config
+
+
+class Beeper(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("world/beezer.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image, (config.DIMENSION_UNIT//2, config.DIMENSION_UNIT//2))
+        self.image = pygame.transform.rotate(self.image, 45)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
