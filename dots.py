2e708428e734d648be0edf9b07c6c851a4e85529
haksoat <shopejuh@gmail.com>
2019-08-03 13:02:36 +0100

    Created Dot object

---
+++ dots.py
@@ -0,0 +1,12 @@
+import config
+import pygame
+
+class Dot(pygame.sprite.Sprite):
+    def __init__(self, center):
+        pygame.sprite.Sprite.__init__(self)
+        width = config.DIMENSION_UNIT / 10
+        height = config.DIMENSION_UNIT / 10
+        self.image = pygame.Surface((width, height))
+        self.image.fill(config.BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = center
